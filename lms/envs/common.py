eba3058408ae899dc594225eed661296a8f60831
Calen Pennington <cale@edx.org>
2015-01-13 09:36:08 -0500

    WIP: add sudo-mode

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1156,6 +1156,10 @@ MIDDLEWARE_CLASSES = (
 
     # catches any uncaught RateLimitExceptions and returns a 403 instead of a 500
     'ratelimitbackend.middleware.RateLimitMiddleware',
+
+    # force re-authentication before activating administrative functions
+    'sudo.middleware.SudoMiddleware',
+
     # needs to run after locale middleware (or anything that modifies the request context)
     'edxmako.middleware.MakoMiddleware',
 
@@ -1811,6 +1815,9 @@ INSTALLED_APPS = (
 
     # Surveys
     'survey',
+
+    # Allows sudo-mode
+    'sudo',
 
     'lms.djangoapps.lms_xblock',
 

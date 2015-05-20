4dcdbb27dc5d582618b3249f382f564cf470f234
Ben McMorran <ben.mcmorran@gmail.com>
2015-05-20 21:02:33 +0000

    Add partially implemented views

--- openedx/core/djangoapps/team_api/serializers.py
+++ openedx/core/djangoapps/team_api/serializers.py
@@ -1,25 +1,46 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import CourseTeam
+from .models import CourseTeam, CourseTeamMembership
+
+class CollapsedUserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='accounts_api', lookup_field='username')
+
+    class Meta:
+        model = User
+        fields = ("username", "url")
 
 
-
-class CourseTeamSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.SerializerMethodField("get_id")
-    course_id = serializers.SerializerMethodField("get_course_id")
-    membership = serializers.SerializerMethodField("get_membership")
-
-    def get_id(self, obj):
-        return obj.course_user_group.name
-
-    def get_course_id(self, obj):
-        return obj.course_user_group.course_id
-
-    def get_membership(self, obj):
-        return obj.course_user_group.users
+class CollapsedCourseTeamSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.CharField(source='team_id')
+    url = serializers.HyperlinkedIdentityField(view_name='teams_detail', lookup_field='team_id')
 
     class Meta:
         model = CourseTeam
-        # This list is the minimal set required by the notification service
+        fields = ("id", "url")
+
+
+class UserMembershipSerializer(serializers.ModelSerializer):
+    user = CollapsedUserSerializer()
+
+    class Meta:
+        model = CourseTeamMembership
+        fields = ("user", "date_joined")
+
+
+class MembershipSerializer(serializers.ModelSerializer):
+    user = CollapsedUserSerializer()
+    team = CollapsedCourseTeamSerializer()
+
+    class Meta:
+        model = CourseTeamMembership
+        fields = ("user", "team", "date_joined")
+
+
+class CourseTeamSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='team_id')
+    membership = UserMembershipSerializer(many=True)
+
+    class Meta:
+        model = CourseTeam
         fields = ("id", "name", "is_active", "course_id", "topic_id", "date_created", "description", "country", "language", "membership")
         # read_only_fields = ("id", "email", "username")

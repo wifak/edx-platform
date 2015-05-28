96e2ae5bb56b2cb778e05554433a18eb5fb0d590
Peter Fogg <pfogg@edx.org>
2015-05-28 15:36:01 -0400

    Clarify that text_search is not supported yet.

--- openedx/core/djangoapps/team_api/tests/test_views.py
+++ openedx/core/djangoapps/team_api/tests/test_views.py
@@ -120,6 +120,8 @@ class TestTeamAPI(APITestCase, ModuleStoreTestCase):
         teams = self.get_teams_list_json(data={'include_inactive': True})
         self.assertEqual(4, teams['count'])
 
+    # Text search is not yet implemented, so this should return HTTP
+    # 400 for now
     def test_list_teams_filter_text_search(self):
         self.get_teams_list(400, data={'text_search': 'foobar'})
 
@@ -333,6 +335,8 @@ class TestTeamAPI(APITestCase, ModuleStoreTestCase):
         self.assertEqual(200, response.status_code)
         self.assertTrue(all([topic['is_active'] for topic in response.data['results']]))
 
+    # Text search is not yet implemented, so this should return HTTP
+    # 400 for now
     def test_list_topics_text_search(self):
         self.client.login(username=self.student_user_enrolled, password=self.test_password)
         params = '?course_id={}&text_search=""'.format(str(self.test_course_1.id))
